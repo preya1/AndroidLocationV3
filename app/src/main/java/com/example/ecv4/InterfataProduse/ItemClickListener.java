c340a66f3152ebf779788e5fb11e21f769be3b8a
Preya <galbu70@gmail.com>
2020-03-20 20:44:45 +0200

    Aplicatie Buna

---
+++ app/src/main/java/com/example/ecv4/InterfataProduse/ItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.ecv4.InterfataProduse;
+
+import android.view.View;
+
+public interface ItemClickListener<onClick>
+{
+    void onClick(View view,int positie,boolean esteClick);
+}
