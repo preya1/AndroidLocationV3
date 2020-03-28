8fc8c9a7e1a0d3587c388267f416dea7a2c4288b
Preya <galbu70@gmail.com>
2020-03-28 17:43:35 +0200

    Bara Navigare Sucess Afisare PRODUSE Setari Adaugare Cos si Vizualizare

--- app/src/main/java/com/example/ecv4/ProdusDetaliiActivity.java
+++ app/src/main/java/com/example/ecv4/ProdusDetaliiActivity.java
@@ -3,12 +3,18 @@ package com.example.ecv4;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.ecv4.Model.Produse;
+import com.example.ecv4.Predominant.Predominant;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,6 +22,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ProdusDetaliiActivity extends AppCompatActivity {
 
@@ -42,6 +52,72 @@ public class ProdusDetaliiActivity extends AppCompatActivity {
 
 
         preiaDetaliiProdus(produsID);
+
+        adaugaCosBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                adaugareCosLista();
+
+            }
+        });
+
+    }
+
+    private void adaugareCosLista()
+    {
+        String salvareTimpCurent,salvareDataCurenta;
+        Calendar dataCalendar = Calendar.getInstance();
+
+        SimpleDateFormat dataCurenta = new SimpleDateFormat("MMM dd,yyyy");
+        salvareDataCurenta = dataCurenta.format(dataCalendar.getTime());
+
+        SimpleDateFormat timpCurent = new SimpleDateFormat("HH:mm:ss a");
+        salvareTimpCurent = timpCurent.format(dataCalendar.getTime());
+
+        final DatabaseReference listaCosRef = FirebaseDatabase.getInstance().getReference().child("Lista Cos");
+
+        final HashMap<String ,Object> cosMap = new HashMap<>();
+        cosMap.put("produsid",produsID);
+        cosMap.put("produsnume",produsNume.getText().toString());
+        cosMap.put("pret",produsPret.getText().toString());
+        cosMap.put("data",salvareDataCurenta);
+        cosMap.put("timp",salvareTimpCurent);
+        cosMap.put("cantitate",numarBtn.getNumber());
+        cosMap.put("discount","");
+
+        listaCosRef.child("Vizualizare Utilizator")
+                .child(Predominant.utilizatorCurent.getTelefon()).child("Produse").child(produsID)
+                .updateChildren(cosMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            listaCosRef.child("Vizualizare Admin")
+                                    .child(Predominant.utilizatorCurent.getTelefon()).child("Produse").child(produsID)
+                                    .updateChildren(cosMap)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task)
+                                        {
+
+                                            if(task.isSuccessful())
+                                            {
+                                                Toast.makeText(ProdusDetaliiActivity.this,"Adaugat in Lista Cos",Toast.LENGTH_LONG).show();
+                                                Intent intent = new Intent(ProdusDetaliiActivity.this,AcasaActivity.class);
+                                                startActivity(intent);
+                                            }
+
+                                        }
+                                    });
+
+                        }
+
+                    }
+                });
 
     }
 
