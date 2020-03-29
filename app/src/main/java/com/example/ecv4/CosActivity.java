c8315848b5b62260f6ff376d5578c28a8d9371ab
Preya <galbu70@gmail.com>
2020-03-29 07:53:08 +0300

    Bara Navigare Sucess Afisare PRODUSE Setari Adaugare Cos si Vizualizare

--- app/src/main/java/com/example/ecv4/CosActivity.java
+++ app/src/main/java/com/example/ecv4/CosActivity.java
@@ -1,22 +1,28 @@
 package com.example.ecv4;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ecv4.Model.Cos;
 import com.example.ecv4.Predominant.Predominant;
 import com.example.ecv4.ProdusVizualizare.CosVizualizare;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -57,11 +63,68 @@ public class CosActivity extends AppCompatActivity {
         FirebaseRecyclerAdapter<Cos, CosVizualizare> adapter
                 = new FirebaseRecyclerAdapter<Cos, CosVizualizare>(options) {
             @Override
-            protected void onBindViewHolder(@NonNull CosVizualizare holder, int position, @NonNull Cos model)
+            protected void onBindViewHolder(@NonNull CosVizualizare holder, int position, @NonNull final Cos model)
             {
-                  holder.produsCantitate.setText( "Cantitate" + model.getCantitate());
+                  holder.produsCantitate.setText( "Cantitate " + model.getCantitate());
                   holder.produsNume.setText(model.getProdusnume());
-                  holder.produsPret.setText( "Pret" + model.getPret() + "RON");
+                  holder.produsPret.setText( "Pret " + model.getPret() + "RON");
+
+                  holder.itemView.setOnClickListener(new View.OnClickListener() {
+                      @Override
+                      public void onClick(View v)
+                      {
+
+                          CharSequence optiuni[] = new CharSequence[]
+                                  {
+                                         "Editare",
+                                         "Stergere"
+                                  };
+
+                          AlertDialog.Builder builder = new AlertDialog.Builder(CosActivity.this);
+                          builder.setTitle("Selectare Optiuni Cos");
+
+                          builder.setItems(optiuni, new DialogInterface.OnClickListener() {
+                              @Override
+                              public void onClick(DialogInterface dialog, int which)
+                              {
+                                  if(which == 0)
+                                  {
+                                      Intent intent = new Intent(CosActivity.this,ProdusDetaliiActivity.class);
+                                      intent.putExtra("produsid",model.getProdusid());
+                                      startActivity(intent);
+
+                                  }
+
+                                  if(which == 1)
+                                  {
+                                     cosListaRef.child("Vizualizare Utilizator")
+                                             .child(Predominant.utilizatorCurent.getTelefon())
+                                             .child("Produse")
+                                             .child(model.getProdusid())
+                                             .removeValue()
+                                             .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                 @Override
+                                                 public void onComplete(@NonNull Task<Void> task)
+                                                 {
+                                                     
+                                                     if(task.isSuccessful())
+                                                     {
+                                                         Toast.makeText(CosActivity.this, "Produs sters cu success !", Toast.LENGTH_SHORT).show();
+
+                                                         Intent intent = new Intent(CosActivity.this,AcasaActivity.class);
+                                                         startActivity(intent);
+                                                     }
+                                                 }
+                                             });
+                                  }
+
+                              }
+                          });
+
+                          builder.show();
+
+                      }
+                  });
             }
 
             @NonNull
